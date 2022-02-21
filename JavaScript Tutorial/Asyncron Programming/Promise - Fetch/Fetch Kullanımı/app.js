36b1f3ef13228f51f6d2ee237184349491c769dd
aydintuna <aydinbahadirtuna@gmail.com>
2022-02-22 00:11:28 +0300

    v

--- JavaScript Tutorial/Asyncron Programming/Promise - Fetch/Fetch Kullanımı/app.js
+++ JavaScript Tutorial/Asyncron Programming/Promise - Fetch/Fetch Kullanımı/app.js
@@ -1,8 +1,24 @@
-function getTextFile() {
+function getTextFile() {    // Text dosyası
     fetch("example.txt")
-    .then(response => console.log(response))
+    .then(response => response.text())
+    .then(data => console.log(data))
     .catch(err => console.log(err));
-
 }
 
-getTextFile();
+function getJsonFile() {    // Local bir json dosyasından veri alma
+    fetch("example.json")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+function getExternalAPI() {
+    fetch("https://v6.exchangerate-api.com/v6/c96c671b626a17923cb824b8/latest/USD")
+    .then(response => response.json())
+    .then(data => console.log(data.conversion_rates.TRY))
+    .catch(err => console.log(err));
+}
+
+// getTextFile();
+// getJsonFile();
+getExternalAPI();
