54d8a8d596744ac9df44bffba9b0133b403fb448
aydintuna <aydinbahadirtuna@gmail.com>
2022-02-24 16:54:48 +0300

    v

---
+++ Projelerim/0 - 100 Tahmin Oyunu/app.js
@@ -0,0 +1,21 @@
+const btnHesapla = document.getElementById("hesapla");
+const btnPc = document.getElementById("pc");
+const btnInsan = document.getElementById("insan");
+const inp1 = document.getElementById("1.kisi");
+const inp2 = document.getElementById("2.kisi");
+
+if (btnPc.value === "pc") {
+    
+}
+
+const goruntule = (e) => {
+    console.log(inp1.value);
+    inp2.value = inp1.value;    
+    e.preventDefault();
+}
+btnHesapla.addEventListener("click",goruntule);
+
+
+
+
+
