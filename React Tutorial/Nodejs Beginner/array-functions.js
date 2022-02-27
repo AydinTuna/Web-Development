32353e17fa14c27e5314998461ea1234f6dfabe0
AydinTuna <tunaaydin6161@gmail.com>
2022-02-27 20:23:58 +0300

    v

---
+++ React Tutorial/Nodejs Beginner/array-functions.js
@@ -0,0 +1,59 @@
+const users = [
+    {
+        name: "Mehmet",
+        age: 10
+    },
+    {
+        name: "Murat",
+        age: 11
+    },    
+    {
+        name: "Sevilay",
+        age: 22
+    }
+];
+
+/*
+    push
+    map
+    find
+    filter
+    some
+    every
+    includes
+*/ 
+
+// push
+// const newArray = users.push("Ayşe");
+
+// console.log(users);
+
+// map
+// users.map((item) => {
+//     console.log(item);
+// })
+
+// find
+// const result = users.find((item) => item.name === "Mehmet" && item.age < 22);
+// console.log(result);
+
+// filter
+// const filtered = users.filter(
+//     item => item.name === "Mehmet" && item.age < 20
+// );
+// console.log(filtered);
+
+// some
+// const some =users.some(item => item.age === 10);
+// console.log(some);
+
+// every
+// const every = users.every(item => item.age > 9);
+// console.log(every);
+// Hepsi 9 dan büyük olduğu için true döndü
+
+// includes
+// const fruits = ["apple","banana","cherry"];
+// const isIncluded = fruits.includes("banana");
+
+// console.log(isIncluded);
