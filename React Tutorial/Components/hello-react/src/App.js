ee2855fb9e0179e1664f267fad55778d57bf4e9d
AydinTuna <tunaaydin6161@gmail.com>
2022-03-06 15:39:11 +0300

    v

--- React Tutorial/Components/hello-react/src/App.js
+++ React Tutorial/Components/hello-react/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+// import React from 'react'; React fonks kullanılmayacaksa componentlerde react import etmeye gerek yoktur
+import Header from './components/Header';
+import User from './components/User';
+
+const name = "Bahadır Tuna";
+const surName = "Aydın";
+const isLoggedIn = false;
 
 function App() {
+  // return React.createElement("div", null, "Hello");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // JSX Fragment
+    <>
+
+      <User />
+      <div>
+        {/* <Header></Header> Bu şekilde de yazılabilir ancak içi dolduralamadığı için tek yazmak daha mantıklı */}
+        <Header />
+      </div>
+      <p>Merhaba</p>
+
+      <label htmlFor='name'>name</label>
+      <input id='name'></input>
+
+      <h1>{name} {surName}</h1>
+      <h1>{`Merhaba benim Adım ${name} ve soyadım ${surName}`}</h1>
+
+      {/* <h1>{isLoggedIn && `Merhaba benim Adım ${name} ve soyadım ${surName}`}</h1>
+      {!isLoggedIn && "Giriş yapmadınız."} */}
+      <h1>{isLoggedIn 
+        ? `Merhaba benim Adım ${name} ve soyadım ${surName}` 
+        : "Giriş yapmadınız."}</h1>
+    </>
   );
 }
 
-export default App;
+export default App; 
